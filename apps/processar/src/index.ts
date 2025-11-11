3fc4d51806e4ed977d5011b3745957cce1004372
Tarak Nath Jana <janatarak739@gmail.com>
2025-11-11 21:42:54 +0530

    create a new microservice -> processor missing same work

--- apps/processar/src/index.ts
+++ apps/processar/src/index.ts
@@ -25,50 +25,54 @@ async function init_kafka_producer(): Promise<Producer> {
 }
 
 const process_all_data = async () => {
-  const chack_all_process = await prisma.out_box_step_run.findMany({
-    where: {},
-    take: 5,
-  
-  });
+  while (true) {
+    try {
+      const chack_all_process = await prisma.out_box_step_run.findMany({
+        where: {},
+        take: 5,
+      });
 
-  let get_producer: Producer;
-  const topic = process.env.KAFKA_TOPIC;
-    // @ts-ignore
-  if (chack_all_process.length !== 0 || !topic) {
-    get_producer = await init_kafka_producer();
-    get_producer.connect();
-  }
-  // @ts-ignore
-  chack_all_process.map((data: any) => {
-    get_producer.send({
+      let get_producer: Producer;
+      const topic = process.env.KAFKA_TOPIC;
       // @ts-ignore
-      topic: topic,
-      messages: [
-        {
+      if (chack_all_process.length !== 0 || !topic) {
+        get_producer = await init_kafka_producer();
+        get_producer.connect();
+      }
+      // @ts-ignore
+      chack_all_process.map((data: any) => {
+        get_producer.send({
+          // @ts-ignore
+          topic: topic,
+          messages: [
+            {
+              // @ts-ignore
+              value: JSON.stringify({
+                type: "MESSAGE_FROM_PROECSSOR",
+                run: data,
+                stage: 1,
+              }),
+            },
+          ],
+        });
+      });
+
+      await prisma.out_box_step_run.deleteMany({
+        where: {
+          id: {
             // @ts-ignore
-          value: JSON.stringify({
-            type:"MESSAGE_FROM_PROECSSOR",
-            run:data,
-            stage:1
-          }),
+            in: chack_all_process.map((data: any) => data.id),
+          },
         },
-      ],
-    });
-  });
+      });
 
-  await prisma.out_box_step_run.deleteMany({
-    where:{
-        id:{
-            // @ts-ignore
-            in:chack_all_process.map((data:any)=>data.id)
-        }
+      console.log("delay : 10s");
+      await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+    } catch (error: any) {
+      console.error(error.messages);
+      process.exit(1);
     }
-  })
-
-  await new Promise((resolve, reject) => setTimeout(resolve, 10000));
-
-
+  }
 };
 
-
-process_all_data()
+process_all_data();
